b89fefefe35ee32cea209d0e3aa865bf49c486dd
Connor Yeager <123019177+Conno26678@users.noreply.github.com>
2025-10-27 14:56:24 -0400

    stuff 10/27

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+//Imports
 const express = require('express');
 const app = express();
 const path = require('path');
 const ejs = require('ejs');
-
 const socketIO = require('socket.io');
 const http = require('http');
 const server = http.createServer(app);
 const io = socketIO(server);
+const port = 3000
 
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+//Middleware
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-const port = 3000
-//add event listeners for socket.io on for listening to (refer to image)
 
+//Routes
 app.get('/', (req, res) => {
   res.render('index.ejs')
 });
@@ -21,14 +31,18 @@ app.get('/game', (req, res) => {
   res.render('game.ejs')
 });
 
-app.get('/customGame', (req, res) => {
-  res.render('customGame.ejs')
-});
+//socket.io listener start thing
 
-app.get('/waitingLobby', (req, res) => {
-  res.render('waitingLobby.ejs')
-});
+// const socket = io(AUTH_URL, {
+//   extraHeaders: {
+//     api: API_KEY
+//   }
+// });
 
+
+//Start the server silly
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
+
+//add event listeners for socket.io on for listening to (refer to image)
